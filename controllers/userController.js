5df7486db6ba4139ba26c64afb7ec83c71578a81
Github Study <scmaster27@gmail.com>
2019-06-09 17:56:15 +0900

    #2.14

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,6 @@
-export const join = (req, res) => res.render("Join");
-export const login = (req, res) => res.render("Login");
-export const logout = (req, res) => res.render("Logout");
-export const users = (req, res) => res.render("Users");
-export const userDetail = (req, res) => res.render("User Detail");
-export const editProfile = (req, res) => res.render("Edit Profile");
-export const changePassword = (req, res) => res.render("Change Password");
+export const join = (req, res) => res.render("join");
+export const login = (req, res) => res.render("login");
+export const logout = (req, res) => res.render("logout");
+export const userDetail = (req, res) => res.render("userDetail");
+export const editProfile = (req, res) => res.render("editProfile");
+export const changePassword = (req, res) => res.render("changePassword");
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,6 @@
 export const home = (req, res) => res.render("home");
-export const search = (req, res) => res.render("Search");
-export const videos = (req, res) => res.render("Videos");
-export const upload = (req, res) => res.render("Upload");
-export const videoDetail = (req, res) => res.render("Video Detail");
-export const editVideo = (req, res) => res.render("Edit Video");
-export const deleteVideo = (req, res) => res.render("Delete Video");
+export const search = (req, res) => res.render("search");
+export const upload = (req, res) => res.render("upload");
+export const videoDetail = (req, res) => res.render("videoDetail");
+export const editVideo = (req, res) => res.render("editVideo");
+export const deleteVideo = (req, res) => res.render("deleteVideo");
