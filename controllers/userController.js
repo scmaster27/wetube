e010f6bfdb7eb0aae0c62b1e64b24cd8291dac2f
Github Study <scmaster27@gmail.com>
2019-06-28 07:14:30 +0900

    #2.23

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,18 @@
-export const join = (req, res) => res.render("join", { pageTitle: "Join" });
+import routes from "../routes";
+
+export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
+export const postJoin = (req, res) => {
+    const{
+        body: {name, email, password, password2}
+    } = req;
+    if(password !== password2){
+        res.status(400);
+        res.render("join", {pageTitle: "Join"});
+    }else{
+        // Todo: Register User, User Login
+        res.redirect(routes.home);
+    }
+}
 export const login = (req, res) => res.render("login", { pageTitle: "Log In" });
 export const logout = (req, res) => res.render("logout", { pageTitle: "Log Out" })
 export const userDetail = (req, res) => res.render("userDetail", { pageTitle: "User Detail" });
